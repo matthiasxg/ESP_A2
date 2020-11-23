c746fdec5853802aadb40dc38af236fea5785e62
Matthias Grill <matthias.grill@student.tugraz.at>
2020-11-23 12:14:56 +0100

    Add square generation

--- a2.c
+++ a2.c
@@ -1,49 +1,89 @@
+//-----------------------------------------------------------------------------
+// a2.c
+//
+// Playfair-Chiffre
+//
+// Group: 9
+//
+// Author: 12007662
+//-----------------------------------------------------------------------------
+//
+
+#include <stdio.h>
 #include "framework.h"
 
+#define ASCII_GAP -32 // a -> A
+
+// forward declarations
+int stringLength(char *text);
+void toUpper(char *text);
+void replaceLetters(char *text, char original, char new_char);
+
 //-----------------------------------------------------------------------------
-// Computes and returns the length of a string 
+///
+/// The main program:
+/// TODO
+///
+/// @return TODO
 //
-// @param text string to check 
+int main()
+{
+  char square[SQUARE_SIDE][SQUARE_SIDE];
+  generatePlayfairSquare(*square);
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+// Computes and returns the length of a string
+//
+// @param text string to check
 //
 // @return length of the string
 //
-int stringLength(char *text);
+int stringLength(char *text)
+{
+  char *index;
+  int length = 0;
+  for (index = text; *index != '\0'; index++)
+  {
+    length++;
+  }
+  return length;
+}
 
 //-----------------------------------------------------------------------------
-// Changes a string to upper case 
+// Changes a string to upper case
 //
 // @param text string to modify
 //
-void toUpper(char *text);
+void toUpper(char *text)
+{
+  char *index;
+  for (index = text; *index != '\0'; index++)
+  {
+    if ('a' <= *index || *index <= 'z')
+    {
+      *index = *index + ASCII_GAP;
+    }
+  }
+}
 
 //-----------------------------------------------------------------------------
-// Replace all the occurences of a letter in a string with another letter 
+// Replace all the occurences of a letter in a string with another letter
 //
 // @param text string to modify
 // @param original letter to replace
 // @param new_char letter used for the substitution
 //
-void replaceLetters(char *text, char original, char new_char);
-
-
-//-----------------------------------------------------------------------------
-int stringLength(char *text) 
+void replaceLetters(char *text, char original, char new_char)
 {
-  // TODO: Implement
-  // WARNING: Do not change the signature of the function
-  return 0;
-}
-
-//-----------------------------------------------------------------------------
-void toUpper(char *text) 
-{
-  // TODO: Implement
-  // WARNING: Do not change the signature of the function
-}
-
-//-----------------------------------------------------------------------------
-void replaceLetters(char *text, char original, char new_char) 
-{
-  // TODO: Implement
-  // WARNING: Do not change the signature of the function
+  char *index;
+  for (index = text; *index != '\0'; index++)
+  {
+    if (*index == original)
+    {
+      *index = new_char;
+    }
+  }
 }
