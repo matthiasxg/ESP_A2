915e3ce598af5bc14f8bf16c9f99d76e304f1398
Matthias Grill <matthias.grill@student.tugraz.at>
2020-11-23 13:40:44 +0100

    Implement Options

--- a2.c
+++ a2.c
@@ -12,10 +12,23 @@
 #include <stdio.h>
 #include "framework.h"
 
+//constants
 #define ASCII_GAP -32 // a -> A
+#define MAX_PLAINTEXT_LENGTH 50
+#define MAX_CIPHERTEXT_LENGTH 50
+#define ENCRYPT 1
+#define DECRYPT 2
+#define QUIT 0
+
+//strings
+#define OPTIONS "1 - Verschlüsseln\n2 - Entschlüsseln\n0 - Programm beenden\n"
+#define PLAINTEXT_MESSAGE "Klartext: "
+#define CIPHERTEXT_MESSAGE "Ciphertext: "
 
 // forward declarations
-
+int getOption();
+void encryptMessage(char *square);
+void decryptMessage(char *square);
 
 //-----------------------------------------------------------------------------
 ///
@@ -29,7 +42,72 @@ int main()
   char square[SQUARE_SIDE][SQUARE_SIDE];
   generatePlayfairSquare(*square);
 
+  int option;
+  while ((option = getOption()) != QUIT)
+  {
+    switch (option)
+    {
+    case ENCRYPT:
+      encryptMessage(*square);
+      break;
+    
+    case DECRYPT:
+      decryptMessage(*square);
+      break;
+    }
+  }
+
   return 0;
+}
+
+//-----------------------------------------------------------------------------
+// TODO
+//
+//
+void encryptMessage(char *square)
+{
+  char plaintext[MAX_PLAINTEXT_LENGTH];
+  do
+  {
+    printf("%s", PLAINTEXT_MESSAGE);
+    fgets(plaintext, MAX_PLAINTEXT_LENGTH, stdin);
+  } while (!checkStringValidity(plaintext, MAX_PLAINTEXT_LENGTH, IS_ENCRYPTING));
+
+  printf("%s\n\n", plaintext);
+  printf("%s", square);
+}
+
+//-----------------------------------------------------------------------------
+// TODO
+//
+//
+void decryptMessage(char *square)
+{
+  char ciphertext[MAX_CIPHERTEXT_LENGTH];
+  do
+  {
+    printf("%s", CIPHERTEXT_MESSAGE);
+    fgets(ciphertext, MAX_CIPHERTEXT_LENGTH, stdin);
+  } while (!checkStringValidity(ciphertext, MAX_PLAINTEXT_LENGTH, IS_DECRYPTING));
+  
+  printf("%s", square);
+}
+
+//-----------------------------------------------------------------------------
+// TODO
+//
+// @return TODO
+//
+int getOption()
+{
+  int option;
+  do
+  {
+    printf("\nWählen Sie bitte eine Option:\n%s\n", OPTIONS);
+    scanf("%i", &option);
+  } while (option != ENCRYPT && option != DECRYPT && option != QUIT);
+  getchar(); // Clear input buffer
+  return option;
 }
 
 //-----------------------------------------------------------------------------
